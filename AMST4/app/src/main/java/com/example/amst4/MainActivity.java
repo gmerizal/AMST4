77377b44899f26502a49551cec652467149b2d54
Guillermo <gmerizal@espol.edu.ec>
2022-06-09 17:55:08 -0500

    modificando menu

--- AMST4/app/src/main/java/com/example/amst4/MainActivity.java
+++ AMST4/app/src/main/java/com/example/amst4/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,6 +43,38 @@ public class MainActivity extends AppCompatActivity {
             Log.d("mensaje","ingreso");
         }else if(view.getId()==R.id.btnRegistro){
 
+        }
+
+
+        }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.calendar_view:
+                // codigo calendario
+                return true;
+            case R.id.video_view:
+                //codigo viddeo
+                return true;
+            case R.id.mapa:
+                // codigo mapa
+                return true;
+            case R.id.grafico:
+                //codigo grafico
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
